8879fe7937e2f8d7cfa3e989f743b540f83e4327
unknown <abdelrahmanahmedhamdy99@gmail.com>
2022-06-19 10:58:31 +0200

    various types of functions in kotlin - not finished yet

---
+++ 07-functions/src/main/kotlin/main.kt
@@ -0,0 +1,64 @@
+fun main() {
+
+    separate("return nothing")
+    sayHelloReturnNothing()
+    var returnUnit = sayHelloReturnNothing() // it's type should be Unit
+    println(returnUnit::class) // Unit
+
+
+    separate("return String")
+    sayHelloReturnString()
+    var returnString: String = sayHelloReturnString()
+    println(returnString::class)
+    println("${sayHelloReturnString()} ")
+
+
+    separate("inner functions")
+    parent()
+
+
+    separate("single line functions")
+    sayHi()
+
+
+    
+
+
+}
+
+// function to separate things in console
+fun separate(title: String = "") {
+    println("\n-----------------$title------------------------")
+}
+
+
+
+// this is how to create simple function
+// default return type of all functions is Unit
+fun sayHelloReturnNothing(): Unit { // also no need for return type Unit -- it automatically happens
+    println("say hello function")
+    return Unit; // no need for this line
+}
+
+// function with return
+fun sayHelloReturnString(): String {
+    val string: String = "someString"
+    return string
+}
+
+// inner functions
+fun parent(){
+    println("i am a parent")
+    fun child() {
+        println("i am a child")
+        fun grandChild() {
+            println("i am a grand child")
+        }
+        grandChild()
+    }
+    child()
+}
+
+
+// single line function
+fun sayHi() = println("Hi")
